75de0204bb2e273712575cb9e3ee4f3a21694d71
Ebediam <54002472+Ebediam@users.noreply.github.com>
2019-08-20 00:29:24 +0200

    Add files via upload

---
+++ ItemModuleShooter.cs
@@ -0,0 +1,22 @@
+﻿using BS;
+
+namespace ItemShooter
+{
+    // This create an item module that can be referenced in the item JSON
+    public class ItemModuleShooter : ItemModule
+    {
+        public string projectileID = "Arrow1";
+        public string bulletSpawnName = "ParryPoint";
+        public string audioName = "None";
+        public string particleSystemName = "None";
+
+        public float projectileSpeed = 40f;
+        public float timeToDespawn = 1f;
+
+        public override void OnItemLoaded(Item item)
+        {
+            base.OnItemLoaded(item);
+            item.gameObject.AddComponent<ItemShooter>();
+        }
+    }
+}
---
+++ ItemShooter.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+using BS;
+
+namespace ItemShooter
+{
+    // The item module will add a unity component to the item object. See unity monobehaviour for more information: https://docs.unity3d.com/ScriptReference/MonoBehaviour.html
+    // This component will apply a force on the player rigidbody to the direction of an item transform when the trigger is pressed (see custom reference in the item definition component of the item prefab)
+    public class ItemShooter : MonoBehaviour
+    {
+        protected Item item;
+
+        public ItemModuleShooter module;
+
+        public Transform bulletSpawn;
+
+        public AudioSource shotSFX = null;
+
+        public ParticleSystem shotVFX = null;
+
+        public bool hasShot = false;
+        ItemData projectileOriginal;
+
+        public bool isShootingAllowed;
+
+
+
+        protected void Awake()
+        {
+            item = this.GetComponent<Item>();
+            module = item.data.GetModule<ItemModuleShooter>();
+
+            isShootingAllowed = true;
+            bulletSpawn = item.transform.Find(module.bulletSpawnName); 
+            
+            if(module.audioName != "None")
+            {
+                shotSFX = item.transform.Find(module.audioName).gameObject.GetComponent<AudioSource>();
+            }
+
+            if(module.particleSystemName != "None")
+            {
+                shotVFX = item.transform.Find(module.particleSystemName).gameObject.GetComponent<ParticleSystem>();
+            }
+            
+            item.OnHeldActionEvent += OnTriggerPressed;
+            projectileOriginal = Catalog.current.GetData<ItemData>(module.projectileID, true);
+            
+        }
+
+        void OnTriggerPressed(Interactor interactor, Handle handle, Interactable.Action action)
+        {
+
+            if (action == Interactable.Action.UseStart && !hasShot && isShootingAllowed)
+            {
+                hasShot = true;
+                Shoot();
+
+            }
+
+
+            if (action == Interactable.Action.UseStop)
+            {
+                hasShot = false;
+
+            }
+
+        }
+
+        void Shoot()
+        {
+            Item projectile = projectileOriginal.Instantiate(null);
+            projectile.gameObject.SetActive(true);
+
+
+            projectile.transform.position = bulletSpawn.transform.position;
+            projectile.transform.rotation = bulletSpawn.transform.rotation;
+
+            if (shotSFX)
+            {
+                shotSFX.Play();
+            }
+
+            if (shotVFX)
+            {
+                shotVFX.Play();
+            }
+
+
+            projectile.Throw(1, Item.FlyDetection.CheckAngle);
+            projectile.GetComponent<Rigidbody>().AddForce(projectile.transform.forward.normalized * module.projectileSpeed, ForceMode.VelocityChange);
+            
+            if(module.timeToDespawn != 0)
+            {
+                Destroy(projectile.gameObject, module.timeToDespawn);
+                Destroy(projectile, module.timeToDespawn);
+            }
+
+        }
+
+    }
+}
