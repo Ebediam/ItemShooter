4c32d88a713bdff7c1a80fa321e41c2d215a7e3b
Ebediam <54002472+Ebediam@users.noreply.github.com>
2019-09-01 11:57:27 +0200

    Add files via upload

--- ItemModuleShooter.cs
+++ ItemModuleShooter.cs
@@ -13,9 +13,7 @@ namespace ItemShooter
         public float projectileSpeed = 40f;
         public float timeToDespawn = 1f;
 
-        public float SFXDelay = 0f;
-        public float VFXDelay = 0f;
-        public float shotDelay = 0f;
+        public float shotDelay = 0.1f;
         public float delayBetweenShots = 1f;
         public bool multipleShotsWithoutReleasingTrigger = false;
 
--- ItemShooter.cs
+++ ItemShooter.cs
@@ -8,99 +8,124 @@ namespace ItemShooter
     public class ItemShooter : MonoBehaviour
     {
         protected Item item;
+
         public ItemModuleShooter module;
+
         public Transform bulletSpawn;
+
         public AudioSource shotSFX = null;
+
         public ParticleSystem shotVFX = null;
+
         public bool hasShot = false;
         ItemData projectileOriginal;
+
         public bool isShootingAllowed;
+
         public bool nextShotReady = true;
+
 
         protected void Awake()
         {
             item = this.GetComponent<Item>();
             module = item.data.GetModule<ItemModuleShooter>();
+
             isShootingAllowed = true;
-            bulletSpawn = item.transform.Find(module.bulletSpawnName);
-            if (module.audioName != "None")
+            bulletSpawn = item.transform.Find(module.bulletSpawnName); 
+            
+            if(module.audioName != "None")
             {
                 shotSFX = item.transform.Find(module.audioName).gameObject.GetComponent<AudioSource>();
             }
-            if (module.particleSystemName != "None")
+
+            if(module.particleSystemName != "None")
             {
                 shotVFX = item.transform.Find(module.particleSystemName).gameObject.GetComponent<ParticleSystem>();
             }
+            
+
             item.OnHeldActionEvent += OnTriggerPressed;
             projectileOriginal = Catalog.current.GetData<ItemData>(module.projectileID, true);
-
+            
         }
 
         void OnTriggerPressed(Interactor interactor, Handle handle, Interactable.Action action)
         {
+
             if (action == Interactable.Action.UseStart && !hasShot && isShootingAllowed && nextShotReady)
             {
+                
                 if (!module.multipleShotsWithoutReleasingTrigger)
                 {
                     hasShot = true;
-                    Invoke("ShootSFX", module.SFXDelay);
-                    Invoke("ShootVFX", module.VFXDelay);
                     Invoke("Shoot", module.shotDelay);
                 }
                 else
                 {
-                    InvokeRepeating("ShootSFX", module.SFXDelay, module.delayBetweenShots);
-                    InvokeRepeating("ShootVFX", module.VFXDelay, module.delayBetweenShots);
                     InvokeRepeating("Shoot", module.shotDelay, module.delayBetweenShots);
                 }
+                
+                
+
+
                 nextShotReady = false;
                 Invoke("Cooldown", module.delayBetweenShots);
+
+                
+
             }
+
 
             if (action == Interactable.Action.UseStop)
             {
+
                 hasShot = false;
                 if (module.multipleShotsWithoutReleasingTrigger)
                 {
-                    CancelInvoke();
+                    CancelInvoke("Shoot");
                 }
+                
+
             }
+
         }
 
         void Cooldown()
         {
             nextShotReady = true;
-        }
 
-        void ShootSFX()
-        {
-            if (shotSFX)
-            {
-                shotSFX.Play();
-            }
-        }
-
-        void ShootVFX()
-        {
-            if (shotVFX)
-            {
-                shotVFX.Play();
-            }
         }
 
         void Shoot()
         {
             Item projectile = projectileOriginal.Instantiate(null);
             projectile.gameObject.SetActive(true);
+
+
             projectile.transform.position = bulletSpawn.transform.position;
             projectile.transform.rotation = bulletSpawn.transform.rotation;
+
+            if (shotSFX)
+            {
+                shotSFX.Play();
+            }
+
+            if (shotVFX)
+            {
+                shotVFX.Play();
+            }
+
+
             projectile.Throw(1, Item.FlyDetection.CheckAngle);
             projectile.GetComponent<Rigidbody>().AddForce(projectile.transform.forward.normalized * module.projectileSpeed, ForceMode.VelocityChange);
-            if (module.timeToDespawn != 0)
+            
+            if(module.timeToDespawn != 0)
             {
                 Destroy(projectile.gameObject, module.timeToDespawn);
                 Destroy(projectile, module.timeToDespawn);
             }
+
         }
+
     }
 }
